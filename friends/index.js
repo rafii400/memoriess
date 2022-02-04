6a5e9fcb577cb69e196c9e6a53e2a7194bd5a83e
hussain rafi <93643287+rafii400@users.noreply.github.com>
2022-02-04 16:09:00 +0600

    Add files via upload

---
+++ friends/index.js
@@ -0,0 +1,37 @@
+$(document).ready(function(){
+
+var count_particles, stats, update;
+stats = new Stats;
+stats.setMode(0);
+stats.domElement.style.position = 'absolute';
+stats.domElement.style.left = '0px';
+stats.domElement.style.top = '0px';
+document.body.appendChild(stats.domElement);
+count_particles = document.querySelector('.js-count-particles');
+update = function() {
+stats.begin();
+stats.end();
+if (window.pJSDom[0].pJS.particles && window.pJSDom[0].pJS.particles.array) {
+count_particles.innerText = window.pJSDom[0].pJS.particles.array.length;
+}
+requestAnimationFrame(update);
+};
+requestAnimationFrame(update);
+
+
+});
+
+
+
+
+
+// img sde//////////
+$(document).ready(function(){
+
+[...document.querySelectorAll('.first-column')].map(column => {
+column.style.setProperty('--animation', 'slide');
+column.style.setProperty('height', '200%');
+column.innerHTML = column.innerHTML + column.innerHTML;
+});
+
+});
